cac8ed183379e2dd278e374168eb8f1182b82100
Surapureddy Amarnath Reddy <73766720+amarnathreddy0201@users.noreply.github.com>
2022-07-02 13:51:44 +0530

    deleted youtube project

--- studybud/base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Room,Message, Topic
-
-admin.site.register(Room)
-admin.site.register(Topic)
-admin.site.register(Message)
