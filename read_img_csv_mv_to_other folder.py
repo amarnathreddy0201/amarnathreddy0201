411e11947fb5081a0329f356411bbd479f1de736
Surapureddy Amarnath Reddy <73766720+amarnathreddy0201@users.noreply.github.com>
2024-01-28 12:06:49 +0530

    Create read_img_csv_mv_to_other folder.py

---
+++ read_img_csv_mv_to_other folder.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+
+excel_file_path = r"train.csv"
+
+# Specify the columns you want to read
+columns_to_read = ['file_name', 'label']
+
+# Read the Excel file, selecting only the specified columns
+train_df = pd.read_csv(excel_file_path, usecols=columns_to_read)
+
+
+
+# loading training images
+train_img = []
+for img_name,label in zip(train_df['file_name'],train_df['label']):
+    dir ="test1"+"/"+str(label)
+    os.makedirs(dir, exist_ok=True)
+
+    source_path = "train/"+img_name
+    if os.path.exists(source_path):
+        destination_path = os.path.join(dir, img_name)
+        # # Rename the file
+        os.rename(source_path, destination_path)
+        
+
