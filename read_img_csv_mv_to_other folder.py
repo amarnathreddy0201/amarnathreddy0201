eb1bf9edba82c005d3a4828380a624c1f7fd792e
Surapureddy Amarnath Reddy <73766720+amarnathreddy0201@users.noreply.github.com>
2024-01-28 21:41:29 +0530

    Update read_img_csv_mv_to_other folder.py
    creating training and testing data from csv file and assign to directory and labeeling.

--- read_img_csv_mv_to_other folder.py
+++ read_img_csv_mv_to_other folder.py
@@ -2,26 +2,62 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
-excel_file_path = r"train.csv"
+def function():
+    excel_file_path = r"train.csv"
+    
+    # Specify the columns you want to read
+    columns_to_read = ['file_name', 'label']
+    
+    # Read the Excel file, selecting only the specified columns
+    train_df = pd.read_csv(excel_file_path, usecols=columns_to_read)
+    
+    
+    
+    # loading training images
+    train_img = []
+    for img_name,label in zip(train_df['file_name'],train_df['label']):
+        dir ="test1"+"/"+str(label)
+        os.makedirs(dir, exist_ok=True)
+    
+        source_path = "train/"+img_name
+        if os.path.exists(source_path):
+            destination_path = os.path.join(dir, img_name)
+            # # Rename the file
+            os.rename(source_path, destination_path)
 
-# Specify the columns you want to read
-columns_to_read = ['file_name', 'label']
+def function1():
+    import os
+    import pandas as pd
+    # from tqdm import tqdm
+    
+    excel_file_path = r"train.csv"
+    
+    # Specify the columns you want to read
+    columns_to_read = ['file_name', 'label']
+    
+    # Read the Excel file, selecting only the specified columns
+    train_df = pd.read_csv(excel_file_path, usecols=columns_to_read)
+    
+    
+    
+    # loading training images
+    train_img = []
+    i=0
+    for img_name,label in zip(train_df['file_name'],train_df['label']):
+        if i%100==0:
+            dir ="test1"+"/"+str(label)
+        else:
+            dir ="train1"+"/"+str(label)
+        os.makedirs(dir, exist_ok=True)
+    
+        source_path = "train/"+img_name
+        if os.path.exists(source_path):
+            destination_path = os.path.join(dir, img_name)
+            # # Rename the file
+            os.rename(source_path, destination_path)
+            i+=1
+        
 
-# Read the Excel file, selecting only the specified columns
-train_df = pd.read_csv(excel_file_path, usecols=columns_to_read)
 
-
-
-# loading training images
-train_img = []
-for img_name,label in zip(train_df['file_name'],train_df['label']):
-    dir ="test1"+"/"+str(label)
-    os.makedirs(dir, exist_ok=True)
-
-    source_path = "train/"+img_name
-    if os.path.exists(source_path):
-        destination_path = os.path.join(dir, img_name)
-        # # Rename the file
-        os.rename(source_path, destination_path)
         
 
