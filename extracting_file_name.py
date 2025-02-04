e07af92e1c6427fa9eca0a74183f4145dadbd383
Surapureddy Amarnath Reddy <73766720+amarnathreddy0201@users.noreply.github.com>
2025-02-04 22:54:06 +0530

    interview

--- extracting_file_name.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-
-images_path = glob.glob("train/*.jpg")
-
-for img in images_path:
-    print(os.path.split(img)[-1])
-    
--- logging_learn.py
+++
@@ -1,9 +0,0 @@
-# Configure the logging module
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(funcName)s:  %(lineno)d - %(message)s  : %(name)s",
-    handlers=[
-        logging.StreamHandler()
-        # Add other handlers as needed, e.g., logging.FileHandler("logfile.log")
-    ],
-)
